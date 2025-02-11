b9952e8c12b4b08eb65d9bcba506a62f09a381f5
Nahuel Piazza <nahuel.piazza@yahoo.com>
2025-02-11 18:51:42 -0300

    se agregaron loaders

--- src/components/ItemListConteiner.jsx
+++ src/components/ItemListConteiner.jsx
@@ -3,31 +3,55 @@ import "./ItemListConteiner.css"
 import { useParams } from "react-router-dom";
 import ItemCard from "./ItemCard";
 import { getCategory, getItems } from "../../AsyncMock";
+import ReactLoading from "react-loading"
 
 export default function ItemListCont () {
     const [Items, setItems] = useState (null);
+    const [loading, setLoading] = useState (true);
     const {catId} = useParams();
 
     useEffect(() => {
         if (!catId) {
-            getItems().then((response) => setItems(response));
+            getItems().then((response) => {
+                setItems(response);
+                setLoading(false);
+            });
         } else {
-            getCategory(catId).then((response) => setItems(response));
+            getCategory(catId).then((response) => {
+                setItems(response);
+                setLoading(false);
+            });
         }
     }, [catId]);
     
     return (
         <>
-            <h1>Vista de Home {catId}</h1>
-            <div style={{
-                display:"flex",
-                flexDirection:"column",
-                gap:"20px",
-                }}>
-                {Items?.map((Item) => (
-                <ItemCard key={Item.id} Item={Item} />
-                ))}
-            </div>
+                
+                { loading ? (
+                    <div style={{display: "flex", flexDirection:"column", justifyContent:"center"}}>
+                    <ReactLoading 
+                    type="bubbles" 
+                    color="blue" 
+                    width="100px" 
+                    height="50px" 
+                    />
+                    </div>
+                )   
+                 :
+                    (
+                    <section>
+                        <h1>{catId}</h1>
+                        <div style={{
+                        display:"flex",
+                        flexDirection:"column",
+                        gap:"20px",
+                        }}>
+                        {Items?.map((Item) => (
+                        <ItemCard key={Item.id} Item={Item} />
+                        ))}
+                        </div>
+                    </section>
+                )}
         </>
     );
 }
