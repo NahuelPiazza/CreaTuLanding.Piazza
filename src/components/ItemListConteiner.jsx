b02618fa490f754358948a9be11e4579f5a6180c
Nahuel Piazza <nahuel.piazza@yahoo.com>
2025-02-28 19:49:43 -0300

    se intenta solucionar error 3

--- src/components/ItemListConteiner.jsx
+++ src/components/ItemListConteiner.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import "./ItemList.css"
 import { useParams } from "react-router-dom";
 import ItemCard from "./ItemCard";
